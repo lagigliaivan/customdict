3dffa3b2435eb46e194bf2de4b76030afa09672e
lagigliaivan <lagigliaivan@gmail.com>
2025-03-28 18:36:18 -0300

    supporting all operations

--- dictionary/dic.go
+++ dictionary/dic.go
@@ -1,5 +1,7 @@
 package dictionary
 
+import "strings"
+
 type Dictionary struct {
 	words map[string]bool
 }
@@ -8,4 +10,32 @@ func New() Dictionary {
 	return Dictionary{
 		words: make(map[string]bool),
 	}
+}
+
+func (d *Dictionary) Add(word string) {
+	d.words[word] = true
+}
+
+func (d *Dictionary) IsPresent(word string) bool {
+	return d.words[word]
+}
+
+func (d *Dictionary) Prefix(prefix string) Dictionary {
+	result := New()
+	for i := range d.words {
+		if strings.Contains(i, prefix) {
+			result.Add(i)
+		}
+	}
+
+	return result
+}
+
+func (d *Dictionary) Remove(word string) Dictionary {
+	delete(d.words, word)
+	return *d
+}
+
+func (d *Dictionary) Len() int {
+	return len(d.words)
 }
--- dictionary/dic_test.go
+++ dictionary/dic_test.go
@@ -3,7 +3,7 @@ package dictionary_test
 import (
 	"testing"
 
-	"github.com/lagigliaivan/customdic/dictionary"
+	"github.com/lagigliaivan/customdict/dictionary"
 )
 
 // Add Word: Add a word to the dictionary.
@@ -24,5 +24,81 @@ func TestAddWordToDictionary(t *testing.T) {
 	if !ok {
 		t.Errorf("Expected %v but got %v", true, ok)
 	}
+}
 
+func TestSearchWordInDictionary(t *testing.T) {
+	dic := dictionary.New()
+
+	dic.Add("hello")
+	dic.Add("world")
+	dic.Add("hello world")
+	dic.Add("world hell")
+	dic.Add("world hello")
+	dic.Add("foo")
+	dic.Add("bar")
+
+	ok := dic.IsPresent("hello")
+	if !ok {
+		t.Errorf("Expected %v but got %v", true, ok)
+	}
+
+	ok = dic.IsPresent("hell")
+	if ok {
+		t.Errorf("Expected %v but got %v", false, ok)
+	}
+}
+
+func TestAutoComplete(t *testing.T) {
+	dic := dictionary.New()
+
+	dic.Add("hello")
+	dic.Add("world")
+	dic.Add("hello world")
+	dic.Add("world hell")
+	dic.Add("world hello")
+	dic.Add("foo")
+	dic.Add("bar")
+
+	words := dic.Prefix("hell")
+	if words.Len() != 4 {
+		t.Errorf("Expected %v but got %v", 4, words.Len())
+	}
+
+	ok := words.IsPresent("hello")
+	if !ok {
+		t.Errorf("Expected %v but got %v", true, ok)
+	}
+
+	ok = words.IsPresent("hello world")
+	if !ok {
+		t.Errorf("Expected %v but got %v", false, ok)
+	}
+
+	ok = words.IsPresent("world hello")
+	if !ok {
+		t.Errorf("Expected %v but got %v", false, ok)
+	}
+
+	ok = words.IsPresent("world hell")
+	if !ok {
+		t.Errorf("Expected %v but got %v", false, ok)
+	}
+}
+
+func TestRemoveWordFromDictionary(t *testing.T) {
+	dic := dictionary.New()
+
+	dic.Add("hello")
+	dic.Add("world")
+	dic.Add("hello world")
+	dic.Add("world hell")
+	dic.Add("world hello")
+	dic.Add("foo")
+	dic.Add("bar")
+
+	dic.Remove("hello")
+	ok := dic.IsPresent("hello")
+	if ok {
+		t.Errorf("Expected %v but got %v", false, ok)
+	}
 }
